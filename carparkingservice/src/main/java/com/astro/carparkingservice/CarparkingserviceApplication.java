834df115f59f49caf2b6cbe2461c283732121351
palsys4 <palsys4@gmail.com>
2020-02-27 17:21:44 +0800

    Add files via upload

---
+++ carparkingservice/src/main/java/com/astro/carparkingservice/CarparkingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.astro.carparkingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarparkingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarparkingserviceApplication.class, args);
+	}
+
+}
---
+++ carparkingservice/src/test/java/com/astro/carparkingservice/CarparkingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astro.carparkingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarparkingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
